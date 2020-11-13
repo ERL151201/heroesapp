b989bdfde7ad9505c7cde47b27021fcb01465515
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-11-13 17:18:20 -0600

    video 222

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -35,4 +35,28 @@ export class HeroesService {
 
   }
 
+  getHeroes(){
+    return this.http.get(`${this.url}/heroes.json`)
+      .pipe(
+        map( this.crearArreglo)
+      );
+  }
+
+  private crearArreglo(heroesObj: object){
+
+    const heroes: HeroeModel [] = [];
+
+      
+    if (heroesObj === null) {return [];}
+
+    Object.keys(heroesObj).forEach(key =>{
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+
+      heroes.push(heroe);
+    });
+
+    return heroes;
+  }
+
 }
